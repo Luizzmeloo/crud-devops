6e8a786edef60718709e02ce807905521dbaaccc
luizzmeloo <lf_m.santos@hotmail.com>
2024-11-12 23:24:00 -0300

    criando get products firebase

--- src/app/demo/service/product.service.ts
+++ src/app/demo/service/product.service.ts
@@ -1,37 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+import { map }from 'rxjs/operators';
 import { Product } from '../api/product';
 
 @Injectable()
 export class ProductService {
 
-    constructor(private http: HttpClient) { }
+    private basePath ="products"
 
-    getProductsSmall() {
-        return this.http.get<any>('assets/demo/data/products-small.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
-    }
+    constructor(private db: AngularFireDatabase) { }
 
-    getProducts() {
-        return this.http.get<any>('assets/demo/data/products.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
+    getProducts(): Observable<Product[]> {
+        return this.db.list<Product>(this.basePath).snapshotChanges().pipe(
+            map(changes => changes.map(c => ({ key: c.payload.key, ...c.payload.val() })))
+        );
     }
-
-    getProductsMixed() {
-        return this.http.get<any>('assets/demo/data/products-mixed.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
+    
+    getProduct(key: string): Observable<Product> {
+        return this.db.object<Product>(`${this.basePath}/${key}`).snapshotChanges().pipe(
+            map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        );
     }
-
-    getProductsWithOrdersSmall() {
-        return this.http.get<any>('assets/demo/data/products-orders-small.json')
-            .toPromise()
-            .then(res => res.data as Product[])
-            .then(data => data);
+    
+    createProduct(product: Product): any {
+        return this.db.list<Product>(this.basePath).push(product);
     }
+    
+    updateProduct(key: string, value: any): Promise<void> {
+        return this.db.object<Product>(`${this.basePath}/${key}`).update(value);
+    }
+    
+    deleteProduct(key: string): Promise<void> {
+        return this.db.object<Product>(`${this.basePath}/${key}`).remove();
+    }
+    
 }
