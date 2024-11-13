0c94ecdc0c9a22a9b05690ec61b4b209f0bccb9b
luizzmeloo <lf_m.santos@hotmail.com>
2024-11-12 22:57:29 -0300

    Firebase configurado com sucesso. 'FIREBASECONFIG E ENVIRONMENT'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
-export const environment = {
-  production: true
+export const environment ={ 
+    production :false,
+    firebaseConfig : {
+    apiKey: "AIzaSyCtfs-VG7JJ5T8N33Z1K85VPN8G_Ucrt_Q",
+    authDomain: "crud-devops-36ea5.firebaseapp.com",
+    databaseURL: "https://crud-devops-36ea5-default-rtdb.firebaseio.com",
+    projectId: "crud-devops-36ea5",
+    storageBucket: "crud-devops-36ea5.firebasestorage.app",
+    messagingSenderId: "361455753407",
+    appId: "1:361455753407:web:197c6faf8a70eac9598e0a",
+    measurementId: "G-V12HH7NX2E"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,20 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+export const environment ={ 
+  production : false, 
+  firebaseConfig : {
+  apiKey: "AIzaSyCtfs-VG7JJ5T8N33Z1K85VPN8G_Ucrt_Q",
+  authDomain: "crud-devops-36ea5.firebaseapp.com",
+  databaseURL: "https://crud-devops-36ea5-default-rtdb.firebaseio.com",
+  projectId: "crud-devops-36ea5",
+  storageBucket: "crud-devops-36ea5.firebasestorage.app",
+  messagingSenderId: "361455753407",
+  appId: "1:361455753407:web:197c6faf8a70eac9598e0a",
+  measurementId: "G-V12HH7NX2E"
+}
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
